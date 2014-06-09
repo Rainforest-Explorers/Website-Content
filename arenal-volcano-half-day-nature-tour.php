9491eb726f84a36f7787a36409be5e7c1856f9e7
Yuliana Rodriguez <info@rainforestexplorers.com>
2014-06-09 10:18:06 -0600

    Update arenal-volcano-half-day-nature-tour.php

--- arenal-volcano-half-day-nature-tour.php
+++ arenal-volcano-half-day-nature-tour.php
@@ -1,1 +1,4 @@
 crear contenido para arenal-volcano-half-day-nature-tour.php
+A perfect scenery to experience wildlife and rainforest, a morning Safari Float is one of the best choices to see most wildlife. The float takes from 2.3 to 3 hours, a perfect choice for the whole family and all ages. A tropical fruit snack will be served by your guide in the middle of the journey through the river.
+Next, we will drive to La Fortuna waterfall to enjoy clear pristine water surrounded by lush rainforest where you can swim and catch unforgettable sceneries in your mind.
+Lunch will be included at the end of this expedition, a local family will be preparing a delicious meal for you. 

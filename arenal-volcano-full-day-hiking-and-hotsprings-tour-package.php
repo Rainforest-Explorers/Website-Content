2396d4d72702e2d1fef5d5ca198861d075c07ef9
Yuliana Rodriguez <info@rainforestexplorers.com>
2014-06-09 08:39:08 -0600

    Update arenal-volcano-full-day-hiking-and-hotsprings-tour-package.php

--- arenal-volcano-full-day-hiking-and-hotsprings-tour-package.php
+++ arenal-volcano-full-day-hiking-and-hotsprings-tour-package.php
@@ -1,1 +1,11 @@
-crear contenido para arenal-volcano-full-day-hiking-and-hotsprings-tour-package.php
+The day starts visiting one of the most important reserves in the area, Arenal Hanging Bridges, offering a 2.5 km trail in the heart of the rainforest in combination with 15 tree top suspension bridges, a unique opportunity to see the forest from a different perspective, walking through the canopy of the trees will allow you to see most of its biodiversity, flora and fauna.
+
+We will continue an exciting journey to La Fortuna Waterfall, a great representation of Costa Rican beauties, the 60m fall creates a natural fresh water pool, perfect for those who love swimming and get in touch with nature, followed by a short ride to enjoy a tasty typical lunch served in an open air area handled by a Costa Rican family.
+
+The tour continues towards the famous Arenal Volcano National Park, combining history and biodiversity. This trail offers the opportunity to walk through an ancient lava flow originated by a big eruption in 1992, at this point you will have amazing views and take great pictures of Arenal Lake and Volcano from the base of it while listening to very interesting information that your local guide will share with you.
+
+Hot Springs will welcome you in the right time, after this exciting day of exploring, you will enjoy a relaxing evening at the hot springs of your preference with a delicious dinner on site.
+
+
+
+

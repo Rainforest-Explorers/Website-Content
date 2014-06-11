aadcc2d95b18007d02276b93d5b3536f7716379e
Yuliana Rodriguez <info@rainforestexplorers.com>
2014-06-11 07:28:14 -0600

    Update arenal-volcano-half-day-hiking-tour.php

--- arenal-volcano-half-day-hiking-tour.php
+++ arenal-volcano-half-day-hiking-tour.php
@@ -1,1 +1,5 @@
-crear contenido para arenal-volcano-half-day-hiking-tour.php
+The day starts exploring Arenal Hanging Bridges reserve, a local guide will join you and explain about the exuberant and lush vegetation while overlooking the canopy of the forest and Arenal Volcano at the distance. It is also very common to find howler monkeys, a lot of birds, and some fauna that your guide will point out.
+Mid morning will be the best time to refresh at La Fortuna Waterfall, a short drive from hanging bridges will encounter clear pristine water that will invite you to jump into the water and enjoy the scenery.
+Next, we will enjoy a tasty typical lunch served in an open air area handled by a Costa Rican family.
+The exciting half day has brought a great experience for everybody, its time to go to relax and enjoy your hotel or any other activity you have planned, we will drop you off at your hotel around 2:30pm.
+

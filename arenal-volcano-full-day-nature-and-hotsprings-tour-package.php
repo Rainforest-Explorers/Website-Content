3c9a309c2b00c77eccc84d11ce9c07ff17aea701
Royner182 <royner182@gmail.com>
2015-03-25 11:40:10 -0600

    Delete arenal-volcano-full-day-nature-and-hotsprings-tour-package.php

--- arenal-volcano-full-day-nature-and-hotsprings-tour-package.php
+++
@@ -1,8 +0,0 @@
-Contenido para arenal-volcano-full-day-nature-and-hotsprings-tour-package.php
-Start the day with a unique experience at Penas Blancas river where a Safari Float will show you the perfect complement between a filled wildlife ecosystem and the sounds of nature.
-Not only exploring but tasting the flavor of our beautiful country is something we do not want you to miss, a tropical fruit snack will be served down river in a magical stop on the river side.
-We will head towards the second exciting destination where you can swim or simply get amazed with the beauty of La Fortuna Waterfall, a perfect place to take pictures, swim in the pristine water, and enjoy the scenery. After this, we will visit a local family that will share a very taste typical lunch with us.
-Next, we will visit Arenal National Park where your knowledgable guide will provide very interesting information of the history, flora, and fauna of this wonderful volcanic earth.
-Continue enjoying Arenal wonders by relaxing at the natural hot springs for a couple hours with an amazing dinner included.
-
-

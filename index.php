a7b33cfcdfcbf836c38630436a9394014076a17e
Royner182 <royner182@gmail.com>
2015-03-25 11:39:35 -0600

    Delete index.php

--- index.php
+++
@@ -1,2 +0,0 @@
-Crear contenido para la pagina principal, por ejemplo Rainforest explorers es una compañia tour operadora fundada en en el area del volcan
-arenal que ofrece tours ecologicos en el area...... Por favor crear bastante contenido para esta area ya que es la que mas leen los clientes.
